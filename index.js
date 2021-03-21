d87cfae3116298e0e633c744514e982472af73a0
Chris <cmlentz76@gmail.com>
2021-03-21 11:59:26 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+// Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Chris is the champion";
